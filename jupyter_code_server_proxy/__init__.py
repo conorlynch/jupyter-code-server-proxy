9583123a9f8cabf4b9e83140bfee3febd0e5c80a
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2022-12-05 14:19:02 +0000

    Put ephemeral scripts in `scratch/bin/<labname>` dir

--- jupyter_code_server_proxy/__init__.py
+++ jupyter_code_server_proxy/__init__.py
@@ -80,7 +80,7 @@ def setup_code_server() -> Dict[str, Any]:
             'auth': 'password',
             'password': code_server_passwd,
             'cert': False,
-            'user-data-dir': os.environ.get('WORK', default=home_dir),
+            'user-data-dir': os.path.join(os.environ.get('WORK', default=home_dir), 'code-server'),
         }
         # Dump config file
         with open(code_server_config_file, 'w') as f:
@@ -131,23 +131,24 @@ CPIDS=''
 wait
 """.format(code_server_executable=code_server_executable)
 
-        # Fall back directory. By default we use JOBSCRATCH to place ephermal
+        # Fall back root directory. By default we use JOBSCRATCH to place ephermal
         # scripts. If this is not available we need to have a smart fallback
         # option to take different users and different JupyterLab instances
         # into account.
         # Fallback to fallback is /tmp/$USER
-        fallback_scratch_dir = os.path.join(
-            os.environ.get(
-                'JUPYTER_CONFIG_DIR', default=os.path.join(
-                    '/tmp', os.environ.get('USER')
-                )
-            ),
+        fallback_scratch_dir_prefix = os.environ.get(
+            'JUPYTER_CONFIG_DIR', default=os.path.join(
+                '/tmp', os.environ.get('USER')
+            )
+        )
+        # Root directory to save the code server wrapper
+        scratch_dir_perfix = os.environ.get(
+            'JOBSCRATCH', default=fallback_scratch_dir_prefix
+        )
+        scratch_dir = os.path.join(
+            scratch_dir_perfix,
             'bin',
             os.environ.get('JUPYTERHUB_SERVER_NAME', default='jupyterlab')
-        )
-        # Directory to save the code server wrapper
-        scratch_dir = os.environ.get(
-            'JOBSCRATCH', default=fallback_scratch_dir
         )
         # Check if scratch dir exists and create one if it does not
         if not os.path.exists(scratch_dir):
